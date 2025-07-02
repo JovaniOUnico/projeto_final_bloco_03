39bacd0caac34b4734194ae1944f555959c5ca24
JovaniOUnico <jovani.souza.almeida@gmail.com>
2025-07-02 19:59:44 -0300

    fix errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,22 @@
-import { ReactNode, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom"
-import { ToastAlerta } from "../utils/ToastAlert";
+import { Link } from "react-router-dom"
 
 function Navbar() {
 
-    const navigate = useNavigate();
-
-    
-    let component: ReactNode
-
-
-    component = (
-
-        <div className='w-full bg-white text-dark-900 shadow-md
-            flex justify-center py-4 shadow-md'>
-
-            <div className="container flex justify-between text-lg">
-                <Link to='/home' className="text-2xl font-bold">Farmacon</Link>
-
-                <div className='flex gap-4'>
-                    <Link to='/categorias' className='hover:underline'>Categorias</Link>
-                    <Link to='/categoria/nova' className='hover:underline'>Cadastrar Categorias</Link>
-                </div>
-            </div>
-        </div>
-
-    )
 
 
 
     return (
         <>
-            { component }
+            <div className='w-full bg-white text-dark-900 shadow-md flex justify-center py-4 shadow-md'>
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Farmacon</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/categorias' className='hover:underline'>Categorias</Link>
+                        <Link to='/categoria/nova' className='hover:underline'>Cadastrar Categorias</Link>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
